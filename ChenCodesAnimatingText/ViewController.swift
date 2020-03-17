dc49061f07b0e37c671d98221f7fd723f860f98f
Chen Codes <chencodes@gmail.com>
2020-03-17 08:37:13 -0700

    Animate text

--- ChenCodesAnimatingText/ViewController.swift
+++ ChenCodesAnimatingText/ViewController.swift
@@ -12,9 +12,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        let timer = Timer.scheduledTimer(withTimeInterval: 4,
+                                         repeats: true) { [weak self] _ in
+                                            self?.updateText()
+                                            self?.animateText()
+        }
+        timer.fire()
     }
-
-
+    
+    private func updateText() {
+        label.text = texts[index % texts.count]
+        label.sizeToFit()
+        index += 1
+    }
+    
+    private func animateText() {
+        label.center = .init(x: Constant.screenWidth + label.frame.width,
+                             y: Constant.screenHalfHeight)
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: { [weak self] in
+                        guard let self = self else { return }
+                        self.label.center = .init(x: Constant.screenWidth/2,
+                                                  y: Constant.screenHalfHeight)
+        }) { _ in
+            UIView.animate(withDuration: 1,
+                           delay: 2,
+                           options: .curveEaseInOut,
+                           animations: { [weak self] in
+                            guard let self = self else { return }
+                            self.label.center = .init(x: -self.label.frame.width,
+                                                      y: Constant.screenHalfHeight)
+            })
+        }
+    }
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 40)
+        return label
+    }()
+    
+    private var index: Int = 0
+    
+    private let texts: [String] = ["First message",
+                                   "Second message",
+                                   "Third message"]
+    
+    private enum Constant {
+        static let screenHalfHeight: CGFloat = UIScreen.main.bounds.height/2
+        static let screenWidth: CGFloat = UIScreen.main.bounds.width
+    }
 }
-
